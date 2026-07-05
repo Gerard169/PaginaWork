3fbf3ccafb0c9d14dabdedfde471b4f153dfb67e
Gerardo <gera_garciag58@unicah.edu>
2026-07-04 21:22:16 -0600

    Compilador

--- mpa/src/data/pages/index_page.js
+++ mpa/src/data/pages/index_page.js
@@ -1,36 +1,28 @@
 export default {
     planes: [
         {
-            card_title: "Plan Free",
+            card_title: "Carta 1",
             card_description: "Tareas básicas y límite de 30 horas vcpu.",
             card_img: "https://picsum.photos/id/156/300/200",
-            card_label: "Free to the Adventure",
+            card_label: "Para novatos",
             card_price: "120.00",
-            card_cta: "Buy",
+            card_cta: "Comprar",
         },
         {
-            card_title: "Professional",
-            card_description: "Free mas 100 Horas vcpu.",
+            card_title: "Carta 2",
+            card_description: "100 horas",
             card_img: "https://picsum.photos/id/256/300/200",
-            card_label: "Startup flight off",
+            card_label: "para profecionales",
             card_price: "1000.00",
-            card_cta: "Buy",
+            card_cta: "Comprar",
         },
         {
-            card_title: "Enterprise",
-            card_description: "Professional plus Unlimited vcpu.",
+            card_title: "Carta 3",
+            card_description: "Mensual.",
             card_img: "https://picsum.photos/id/56/300/200",
-            card_label: "Serious Business",
+            card_label: "negocio serio",
             card_price: "50,000.00",
-            card_cta: "Buy",
-        },
-        {
-            card_title: "On Premise",
-            card_description: "All on your servers.",
-            card_img: "https://picsum.photos/id/58/300/200",
-            card_label: "Must schedule a call with an advisor",
-            card_price: "???",
-            card_cta: "Buy",
+            card_cta: "Comprar",
         }
     ]
 };
